d02da48d0c0f07e003344ff90e6c1e3d5cc42451
Stack <gambill.staci@gmail.com>
2026-03-16 15:19:16 -0400

    Create trigger.lang.php

---
+++ inc/languages/english/trigger.lang.php
@@ -0,0 +1,24 @@
+<?php
+// general
+$l['trigger_trigger'] = 'Trigger';
+$l['trigger_examples'] = 'e.g., death, suicide';
+
+// trigger overlay thread
+$l['trigger_backToIndex'] = 'Back to Index';
+$l['trigger_readThread'] = 'Read Topic';
+$l['trigger_warning'] = 'Warning!';
+$l['trigger_warningText'] = 'This topic contains subjects that you have marked as triggers. <br>Specifically, these are:';
+
+// new thread
+$l['trigger_newthread'] = 'Trigger<br>Also take inspiration from <a href="misc.php?action=trigger" target="_blank">dieser Seite</a>';
+
+// redirect
+$l['trigger_redirect'] = 'Your changes have been saved.';
+
+// trigger overview site
+$l['trigger_tabOverview'] = 'Overview';
+$l['trigger_tabManage'] = 'Manage Triggers';
+$l['trigger_overviewName'] = 'Name';
+$l['trigger_overviewTrigger'] = 'Trigger';
+$l['trigger_manageInfo'] = 'Enter your triggers here. Please ensure that you use keywords and use the exact same wording as others (e.g., graphic violence instead of graphic-violence).';
+$l['trigger_manageButton'] = 'Save';
